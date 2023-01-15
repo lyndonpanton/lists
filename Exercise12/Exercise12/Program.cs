0aef7e0ea8399822ce614353354b6cf39bdcc5ed
Lyndon Panton <LyndonPanton@gmail.com>
2023-01-15 03:09:44 +0000

    Imported the supplied starter files

---
+++ Exercise12/Exercise12/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using ConsoleCards;
+
+namespace Exercise12
+{
+    /// <summary>
+    /// Exercise 12
+    /// </summary>
+    class Program
+    {
+        /// <summary>
+        /// Exercise 12
+        /// </summary>
+        /// <param name="args">command-line args</param>
+        static void Main(string[] args)
+        {
+            // create deck and hand
+            Deck deck = new Deck();
+            Card[] hand = new Card[5];
+
+            deck.Shuffle();
+
+            // add card, flip over, and print
+            hand[0] = deck.TakeTopCard();
+            hand[0].FlipOver();
+            hand[0].Print();
+            Console.WriteLine();
+
+            // add another card, flip over, and print both cards
+            hand[1] = deck.TakeTopCard();
+            hand[1].FlipOver();
+            hand[1].Print();
+            hand[0].Print();
+        }
+    }
+}
