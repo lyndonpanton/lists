f6a6dd67780e1836226f1af2894033ee5d96a793
Lyndon Panton <LyndonPanton@gmail.com>
2023-01-15 03:15:52 +0000

    Completed problem 1
    Changed the array of cards to a List of card
    Use the Add method instead of indexing to add cards to the object

--- Exercise12/Exercise12/Program.cs
+++ Exercise12/Exercise12/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using ConsoleCards;
 
 namespace Exercise12
@@ -16,18 +17,21 @@ namespace Exercise12
         {
             // create deck and hand
             Deck deck = new Deck();
-            Card[] hand = new Card[5];
+            //Card[] hand = new Card[5];
+            List<Card> hand = new List<Card>();
 
             deck.Shuffle();
 
             // add card, flip over, and print
-            hand[0] = deck.TakeTopCard();
+            //hand[0] = deck.TakeTopCard();
+            hand.Add(deck.TakeTopCard());
             hand[0].FlipOver();
             hand[0].Print();
             Console.WriteLine();
 
             // add another card, flip over, and print both cards
-            hand[1] = deck.TakeTopCard();
+            //hand[1] = deck.TakeTopCard();
+            hand.Add(deck.TakeTopCard());
             hand[1].FlipOver();
             hand[1].Print();
             hand[0].Print();
